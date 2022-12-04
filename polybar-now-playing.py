824e0ff5e18dba2fbc9d67c9aa10faeee913c757
Mukul Debnath <debnath.mukul@gmail.com>
2022-12-05 04:17:23 +0530

    get rid of Playectl dependency check

--- polybar-now-playing.py
+++ polybar-now-playing.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3 -u
 
-import gi
 import json
-gi.require_version('Playerctl', '2.0')
-from gi.repository import Playerctl, GLib
+
+from gi.repository import GLib, Playerctl
 
 ICON_TITLE = ""
 ICON_PLAY = ""
@@ -15,6 +14,7 @@ MAX_LENGTH_TITLE = 40
 MAX_LENGTH_ARTIST = 25
 
 manager = Playerctl.PlayerManager()
+
 
 def print_weather_output():
     with open("/home/mukul/.cache/weather.json", "rt") as f:
@@ -49,7 +49,18 @@ def print_weather_output():
             "Tornado",
         ]:
             weather_icon = " "
-        print("%{T3}" + weather_icon + "%{T-}  " + w_temp + "  " + w_condition + " in " + w_city + ", " + w_country)
+        print(
+            "%{T3}"
+            + weather_icon
+            + "%{T-}  "
+            + w_temp
+            + "  "
+            + w_condition
+            + " in "
+            + w_city
+            + ", "
+            + w_country
+        )
 
 
 def print_music_output(player):
@@ -61,9 +72,37 @@ def print_music_output(player):
         if len(title) > MAX_LENGTH_TITLE:
             title = title[:MAX_LENGTH_TITLE] + "..."
         if player.props.status == "Playing":
-            print("%{T3}" + ICON_TITLE + "%{T-}  " + artist + " - " + title + "  %{T3}%{A1:playerctl previous:}" + ICON_PREVIOUS + "%{A}  %{A1:playerctl play-pause:}" + ICON_PAUSE + "%{A}  %{A1:playerctl next:}" + ICON_NEXT + "%{A}%{T-}")
+            print(
+                "%{T3}"
+                + ICON_TITLE
+                + "%{T-}  "
+                + artist
+                + " - "
+                + title
+                + "  %{T3}%{A1:playerctl previous:}"
+                + ICON_PREVIOUS
+                + "%{A}  %{A1:playerctl play-pause:}"
+                + ICON_PAUSE
+                + "%{A}  %{A1:playerctl next:}"
+                + ICON_NEXT
+                + "%{A}%{T-}"
+            )
         else:
-            print("%{T3}" + ICON_TITLE + "%{T-}  " + artist + " - " + title + "  %{T3}%{A1:playerctl previous:}" + ICON_PREVIOUS + "%{A}  %{A1:playerctl play-pause:}" + ICON_PLAY + "%{A}  %{A1:playerctl next:}" + ICON_NEXT + "%{A}%{T-}")
+            print(
+                "%{T3}"
+                + ICON_TITLE
+                + "%{T-}  "
+                + artist
+                + " - "
+                + title
+                + "  %{T3}%{A1:playerctl previous:}"
+                + ICON_PREVIOUS
+                + "%{A}  %{A1:playerctl play-pause:}"
+                + ICON_PLAY
+                + "%{A}  %{A1:playerctl next:}"
+                + ICON_NEXT
+                + "%{A}%{T-}"
+            )
     else:
         print_weather_output()
 
@@ -80,13 +119,20 @@ def on_metadata(player, metadata, manager):
     print_music_output(player)
 
 
+def on_name_appeared(manager, name):
+    init_player(name)
+
+
 def init_player(name):
     player = Playerctl.Player.new_from_name(name)
-    player.connect('playback-status::playing', on_play, manager)
-    player.connect('playback-status::paused', on_pause, manager)
-    player.connect('metadata', on_metadata, manager)
+    player.connect("playback-status::playing", on_play, manager)
+    player.connect("playback-status::paused", on_pause, manager)
+    player.connect("metadata", on_metadata, manager)
     manager.manage_player(player)
     print_music_output(player)
+
+
+manager.connect("name-appeared", on_name_appeared)
 
 for name in manager.props.player_names:
     init_player(name)
