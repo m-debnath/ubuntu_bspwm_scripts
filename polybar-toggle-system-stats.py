b9c3fb2d9a94ac8258cb0dd0a740e80d2b12e12d
Mukul Debnath <debnath.mukul@gmail.com>
2022-12-10 03:59:56 +0530

    Polybar Toggle Stats: Hidden everything except check_updates

--- polybar-toggle-system-stats.py
+++ polybar-toggle-system-stats.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
-import re
-
 with open("/home/mukul/.config/polybar/config", "rt") as f:
     data = f.read()
 
-if "cpu gpu-usage memory" in data and "memory filesystem homefilesystem check_update" in data:
-    result = data.replace("cpu gpu-usage memory", "cpu memory").replace(
-        "memory filesystem homefilesystem check_update", "memory check_update"
+
+if (
+    "xkeyboard sep sep sep-primary cpu gpu-usage memory filesystem homefilesystem check_update"
+    in data
+):
+    result = data.replace(
+        "xkeyboard sep sep sep-primary cpu gpu-usage memory filesystem homefilesystem check_update",
+        "xkeyboard sep sep sep-primary check_update",
     )
-elif "cpu memory" in data and "memory check_update" in data:
-    result = data.replace("cpu memory", "cpu gpu-usage memory").replace(
-        "memory check_update", "memory filesystem homefilesystem check_update"
+elif "xkeyboard sep sep sep-primary check_update" in data:
+    result = data.replace(
+        "xkeyboard sep sep sep-primary check_update",
+        "xkeyboard sep sep sep-primary cpu gpu-usage memory filesystem homefilesystem check_update",
     )
+
+
+# if "cpu gpu-usage memory" in data and "memory filesystem homefilesystem check_update" in data:
+#     result = data.replace("cpu gpu-usage memory", "cpu memory").replace(
+#         "memory filesystem homefilesystem check_update", "memory check_update"
+#     )
+# elif "cpu memory" in data and "memory check_update" in data:
+#     result = data.replace("cpu memory", "cpu gpu-usage memory").replace(
+#         "memory check_update", "memory filesystem homefilesystem check_update"
+#     )
 
 with open("/home/mukul/.config/polybar/config", "wt") as f:
     f.write(result)
