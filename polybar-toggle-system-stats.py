b27f5bc63d16b7af28aebdd20b2eebbbcc765def
Mukul Debnath <debnath.mukul@gmail.com>
2022-12-13 05:28:40 +0530

    toggle-system-stats: cleanup

--- polybar-toggle-system-stats.py
+++ polybar-toggle-system-stats.py
@@ -20,15 +20,5 @@ elif "xkeyboard sep sep sep-primary check_update" in data:
 else:
     result = data
 
-
-# if "cpu gpu-usage memory" in data and "memory filesystem homefilesystem check_update" in data:
-#     result = data.replace("cpu gpu-usage memory", "cpu memory").replace(
-#         "memory filesystem homefilesystem check_update", "memory check_update"
-#     )
-# elif "cpu memory" in data and "memory check_update" in data:
-#     result = data.replace("cpu memory", "cpu gpu-usage memory").replace(
-#         "memory check_update", "memory filesystem homefilesystem check_update"
-#     )
-
 with open("/home/mukul/.config/polybar/config", "wt") as f:
     f.write(result)
