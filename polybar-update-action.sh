c17396a33b49dbe781a0e58e55fcd11ebb7db19b
Mukul Debnath <debnath.mukul@gmail.com>
2022-12-15 01:14:48 +0530

    polybar-update-action: Updated captions.

--- polybar-update-action.sh
+++ polybar-update-action.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gnome-terminal -e 'sh -c "echo Synchronizing Packages; echo ----------------------;sudo apt update; echo ;echo Starting Package Upgrade; echo ------------------------; sudo apt upgrade; echo ;echo Refreshing Cache; echo ----------------; check-updates.py; echo ; echo System update finished.; echo -----------------------; exec zsh"'
+gnome-terminal -e 'sh -c "echo Synchronizing Packages; echo ----------------------;sudo apt update; echo ;echo Starting Software Update; echo ------------------------; sudo apt upgrade; echo ;echo Refreshing Cache; echo ----------------; check-updates.py; echo ; echo Software Update finished.; echo -------------------------; exec zsh"'
